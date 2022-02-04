f0ed238dc36c909d535910c57b3cef8a627b24c0
w9048521 <w9048521@live.tees.ac.uk>
2022-02-04 13:57:13 +0000

    add fragment transaction

--- app/src/main/java/com/example/fragmentdemo/MainActivity.java
+++ app/src/main/java/com/example/fragmentdemo/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.fragmentdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,11 +15,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
+            fragmentManager = getSupportFragmentManager();
+
+            fragmentManager.beginTransaction()
                     .setReorderingAllowed(true)
                     .add(R.id.fragment_container_view, FirstFragment.class, null)
                     .commit();
         }
+
+    }
+
+    public void replaceFragment(View view){
+
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view, SecondFragment.class, null)
+                .setReorderingAllowed(true)
+                .addToBackStack("Second Fragment") // name can be null
+                .commit();
 
     }
 }
